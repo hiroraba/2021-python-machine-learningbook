f4c8d1dacd4d29fb51482506e5957e7fed17aeb6
hiroraba <xornelius0313@gmail.com>
2021-06-13 16:47:58 +0900

    20210613

--- ch03/scikit_Perceptron.py
+++ ch03/scikit_Perceptron.py
@@ -41,7 +41,7 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 
 # 訓練データの平均と標準偏差を計算
-sc.fit(X_train)
+sc.fit(X_train) 
 
 # 平均と標準偏差から、データを標準化
 X_train_std = sc.transform(X_train)
