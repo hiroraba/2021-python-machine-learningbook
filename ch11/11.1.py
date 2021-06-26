f5a20267507dbd34c5166d0822ef3f0d246c522e
hiroraba <xornelius0313@gmail.com>
2021-06-27 01:13:55 +0900

    20210627

--- ch11/11.1.py
+++ ch11/11.1.py
@@ -2,6 +2,7 @@
 
 # 2次元のデータセットを準備する
 from scipy.sparse.construct import random
+from sklearn import metrics
 from sklearn.datasets import make_blobs
 X, y = make_blobs(n_samples=150, # データ点の数
                   n_features=2, # 特徴量
@@ -61,5 +62,75 @@ plt.scatter(km.cluster_centers_[:, 0],
             label='Centroids')
 plt.legend(scatterpoints=1)
 plt.grid()
+plt.tight_layout()
+plt.show()
+
+# クラスタ内誤差平方和（歪み）
+print('Distortion: %.2f' % km.inertia_)
+
+distortions = []
+for i in range(1, 11):
+    km = KMeans(n_clusters=i,
+                init='k-means++',
+                n_init=10,
+                max_iter=300,
+                random_state=0)
+    km.fit(X)
+    distortions.append(km.inertia_)
+    
+plt.plot(range(1,11), distortions, marker='o')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Distortion')
+plt.tight_layout()
+plt.show()
+
+
+""" 11.1.5 - シルエット図を使ってクラスタリングの性能を数値化する """
+
+# シルエット係数の計算手順
+# 1 クラスタの凝集度を計算する。凝集度は、クラスタのデータ点xと他の全データ点との平均距離として計算する
+# 2 最も近いクラスタからの乖離度bを計算する。乖離度は、データ点xと最も近いクラスタ内の全データ点との平均距離として計算する
+# 3 クラスタの凝集度と乖離度の差を大きい方の数値で割る
+#  - 乖離度 >>> 凝集度の場合に1に近づく
+#  - 乖離度により、他のクラスタとの非類似度が数値化され、凝集度によって同じクラスタ内の他のデータ点ての類似度が数値化されるため
+
+# シルエット係数の可視化
+km = KMeans(n_clusters=3,
+            init='k-means++',
+            n_init=10,
+            max_iter=300,
+            tol=1e-04,
+            random_state=0)
+y_km = km.fit_predict(X)
+
+import numpy as np
+from matplotlib import cm
+from sklearn.metrics import silhouette_samples
+cluster_labels = np.unique(y_km)
+n_clusters = cluster_labels.shape[0]
+
+# シルエット係数計算
+silhouette_vals = silhouette_samples(X, y_km, metric='euclidean')
+y_ax_lower, y_ax_upper = 0,0
+yticks = []
+
+for i, c in enumerate(cluster_labels):
+    c_silhouette_vals = silhouette_vals[y_km == c]
+    c_silhouette_vals.sort()
+    y_ax_upper += len(c_silhouette_vals)
+    color = cm.jet(float(i)/n_clusters)
+    plt.barh(range(y_ax_lower, y_ax_upper),
+             c_silhouette_vals,
+             height=1.0,
+             edgecolor='none',
+             color=color)
+    yticks.append((y_ax_lower + y_ax_upper) / 2.) # クラスタラベルの表示位置
+    y_ax_lower += len(c_silhouette_vals)
+    
+silhouette_avg = np.mean(silhouette_vals)
+plt.axvline(silhouette_avg, color='red', linestyle='--')
+plt.yticks(yticks, cluster_labels + 1)
+plt.ylabel('Cluster')
+plt.xlabel('Silhouette coefficient')
 plt.tight_layout()
 plt.show()
---
+++ ch11/11.2.py
@@ -0,0 +1,75 @@
+""" 11.2 - クラスタを階層木として構成する """
+
+# 完全連結法に基づく凝集型階層的クラスタリングの手順
+# 1 すべてのデータ点の距離行列を計算する
+# 2 各データ点を単一のクラスタとみなして表現する
+# 3 最も類似度の低いメンバーの距離に基づき、2つの最も近いクラスタをマージする
+# 4 距離行列を更新する
+# 5 クラスタが一つだけに残った状態になるまで、手順3-4を繰り返す
+
+import matplotlib.pyplot as plt
+from numpy.core.defchararray import index
+import pandas as pd
+import numpy as np
+np.random.seed(123)
+
+# ランダムなデータ点を作成
+## 特徴量
+variables = ['X', 'Y', 'Z']
+## クラスラベル
+labels = ['ID_0', 'ID_1', 'ID_2', 'ID_3', 'ID_4']
+X = np.random.random_sample([5,3])*10 # 5行3列のサンプルデータを作成
+df = pd.DataFrame(X, columns=variables, index=labels)
+print(df)
+
+# 距離行列を計算する
+from scipy.spatial.distance import pdist, squareform
+row_dist = pd.DataFrame(squareform(pdist(X, metric='euclidean')), columns=labels, index=labels)
+
+# 完全連結法に基づく凝集型階層的クラスタリング
+from scipy.cluster.hierarchy import linkage
+row_clusters = linkage(df.values, method='complete', metric='euclidean')
+print(pd.DataFrame(row_clusters,
+                   columns=['row label1',
+                            'row label2',
+                            'distance',
+                            'no. of items in cluster.'],
+                   index=['cluster %d' % (i + 1) for i in range(row_clusters.shape[0])]))
+
+# 1列目、2列目は各クラスタにおける一番類似度の低いメンバーのインデックスを表す
+# - ここで作られたクラスタは新しいインデックス5が振られる（元のデータが0~4のため）
+# - クラスタ番号が増えるにつれ、各メンバーがクラスタにまとめられていく
+# - 最終的にクラスタ内のメンバー数＝元のメンバー数（この場合5）になったら学習終了。
+
+# 樹形図として表す
+from scipy.cluster.hierarchy import dendrogram
+row_dendr = dendrogram(row_clusters, labels=labels)
+plt.ylabel('Euclidean distance')
+plt.tight_layout()
+plt.show()
+
+""" 11.2.3 - 樹形図をヒートマップと組み合わせる """
+fig = plt.figure(figsize=(8, 8), facecolor='white')
+axd = fig.add_axes([0.09, 0.1, 0.2, 0.6])
+row_dendr = dendrogram(row_clusters, orientation='left')
+
+# クラスタリングのラベルにアクセスするにはleavesを使う
+df_rowclust = df.iloc[row_dendr['leaves'][::-1]]
+
+axm = fig.add_axes([0.23, 0.1, 0.6, 0.6])
+cax = axm.matshow(df_rowclust, interpolation='nearest', cmap='hot_r')
+
+plt.show()
+
+""" 11.2.4 - scikit-learnを使って凝集型階層的クラスタリングを適用する """
+from sklearn.cluster import AgglomerativeClustering
+
+ac = AgglomerativeClustering(n_clusters=3,
+                             affinity='euclidean',
+                             linkage='complete') # 連結法（ここでは完全連結法）
+
+labels = ac.fit_predict(X)
+print('Cluster labels: %s' % labels)
+# ID_0, 4が1つ目のクラスタ(ラベル1)に割当られている
+# ID_1, 2が2つめのクラスタ(ラベル0)に割り当てられている
+# ID_3は独自のクラスタに割当られている
